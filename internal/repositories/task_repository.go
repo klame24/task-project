24bd87c749069a86b24874ac90d82abd889062ba
Kirill Yurchenko <kirya.yurchenko.1996@mail.ru>
2026-01-04 14:45:06 +0300

    add task model realize CreateTask DeleteTask GetAllTasks GetTaskByID in task-repository make migrations v1 up | down make makefile

---
+++ internal/repositories/task_repository.go
@@ -0,0 +1,120 @@
+package repositories
+
+import (
+	"context"
+	"task-project/internal/models"
+	"time"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type TaskRepository interface {
+	CreateTask(ctx context.Context, task *models.Task) error // Добавьте ctx
+	UpdateTask()
+	DeleteTask(ctx context.Context, taskID int) error
+	GetAllTasks(ctx context.Context) ([]*models.Task, error)
+	GetTaskByID(ctx context.Context, taskID int) (*models.Task, error)
+}
+
+type taskRepository struct {
+	ctx context.Context
+	db  *pgx.Conn
+}
+
+func NewTaskRepository(db *pgx.Conn) TaskRepository {
+	return &taskRepository{
+		db: db,
+	}
+}
+
+func (r *taskRepository) CreateTask(ctx context.Context, task *models.Task) error {
+	task.Completed = false
+	task.CreatedAt = time.Now()
+
+	sqlQuery := `INSERT INTO tasks(title, description, completed, created_at)
+	VALUES ($1, $2, $3, $4);`
+
+	_, err := r.db.Exec(
+		ctx,
+		sqlQuery,
+		task.Title,
+		task.Description,
+		task.Completed,
+		task.CreatedAt,
+	)
+
+	return err
+}
+
+func (r *taskRepository) UpdateTask() {}
+
+func (r *taskRepository) DeleteTask(ctx context.Context, taskID int) error {
+	sqlQuery := `DELETE FROM tasks WHERE tasks.id = $1`
+	_, err := r.db.Exec(
+		ctx,
+		sqlQuery,
+		taskID,
+	)
+
+	return err
+}
+
+func (r *taskRepository) GetAllTasks(ctx context.Context) ([]*models.Task, error) {
+	sqlQuery := `
+		SELECT * FROM tasks;
+	`
+
+	rows, err := r.db.Query(ctx, sqlQuery)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var tasks []*models.Task
+
+	for rows.Next() {
+		task := &models.Task{}
+		err := rows.Scan(
+			&task.ID,
+			&task.Title,
+			&task.Description,
+			&task.Completed,
+			&task.CreatedAt,
+		)
+		if err != nil {
+			return nil, err
+		}
+
+		tasks = append(tasks, task)
+	}
+
+	return tasks, nil
+}
+
+func (r *taskRepository) GetTaskByID(ctx context.Context, taskID int) (*models.Task, error) {
+	sqlQuery := `
+		SELECT * FROM tasks
+		WHERE tasks.id = $1
+	`
+
+	row, err := r.db.Query(ctx, sqlQuery, taskID)
+	if err != nil {
+		return nil, err
+	}
+
+	task := &models.Task{}
+
+	err = row.Scan(
+		&task.ID,
+		&task.Title,
+		&task.Description,
+		&task.Completed,
+		&task.CreatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return task, nil
+}
---
+++ migrations/000001_create_tasks_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS tasks (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL UNIQUE,
+    description VARCHAR(1000),
+    completed BOOLEAN NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW()
+)
