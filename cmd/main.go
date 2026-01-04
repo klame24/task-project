2543563aa2300758232254293121c95d06d78b65
Kirill Yurchenko <kirya.yurchenko.1996@mail.ru>
2026-01-04 15:20:36 +0300

    basic structue project

--- cmd/main.go
+++ cmd/main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
 	"task-project/internal/database"
+	"task-project/internal/handlers"
+	"task-project/internal/repositories"
+	"task-project/internal/routes"
+	"task-project/internal/services"
 
 	"github.com/joho/godotenv"
 )
@@ -24,12 +29,25 @@ func main() {
 	fmt.Println("Connect to DB is successfully")
 	fmt.Println(conn)
 	// инициализация репозитория
-
+	taskR := repositories.NewTaskRepository(conn)
 
 	// инициализация сервиса, в который мы передаем репозиторий и сервис управляет какие данные мы выдаем пользователю
+	taskS := services.NewTaskService(taskR)
+	title := "test 2"
+	descr := "test 2 descr"
 
+	_, err = taskS.CreateTask(ctx, title, descr)
+	if err != nil {
+		fmt.Println("Не получилось создать таску из сервиса")
+	}
+	fmt.Println("Получилось создать таску из сервиса!")
 	// инициализация маршрутов
-
+	taskH := handlers.NewTaskHandler(taskS)
 	// запуск сервера
+	router := routes.SetupRoutes(taskH)
 
+	err = http.ListenAndServe(":5050", router)
+	if err != nil {
+		panic(err)
+	}
 }
